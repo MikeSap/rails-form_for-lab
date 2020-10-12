0d9a6c0e81bf3921f1ec31535af134db74e60ce5
mike <mikesapienzaaudio@gmail.com>
2020-10-11 20:04:57 -0500

    14 pending 0 failures

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :school_classes, only: [:index, :show, :create, :new, :update, :edit]
+
+  resources :students, only: [:index, :show, :create, :new, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
